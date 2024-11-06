0cd3bab523f99591024beb72b04ca0231e6009c6
aannddrree <aannddrree.silva@gmail.com>
2024-11-06 19:42:52 -0300

    v1

---
+++ ex3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int numeros[5] = {1,2,3,4,5};
+    int soma = 0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        soma += numeros[i];
+    }
+
+    printf("A Soma e: %d", soma);
+    return 0;
+}
